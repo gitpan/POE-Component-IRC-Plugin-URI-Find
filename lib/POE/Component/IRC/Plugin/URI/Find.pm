18ed6689ad4c5291bc8fdf7e773b44bd7eba40b5
Chris Williams <chris@bingosnet.co.uk>
2008-05-13 10:02:36 +0000

    Import of BINGOS/POE-Component-IRC-Plugin-URI-Find-1.06 from CPAN.
    gitpan-cpan-distribution: POE-Component-IRC-Plugin-URI-Find
    gitpan-cpan-version:      1.06
    gitpan-cpan-path:         BINGOS/POE-Component-IRC-Plugin-URI-Find-1.06.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/POE/Component/IRC/Plugin/URI/Find.pm
+++ lib/POE/Component/IRC/Plugin/URI/Find.pm
@@ -7,7 +7,7 @@ use POE::Component::IRC::Plugin qw(:ALL);
 use URI::Find;
 use vars qw($VERSION);
 
-$VERSION = '1.04';
+$VERSION = '1.06';
 
 sub new {
   my $package = shift;
@@ -168,6 +168,12 @@ With the following parameters:
 =head1 AUTHOR
 
 Chris 'BinGOs' Williams
+
+=head1 LICENSE
+
+Copyright C<(c)> Chris Williams.
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
 =head1 SEE ALSO
 
